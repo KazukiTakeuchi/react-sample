e0e22aa0ab9741c1edc44dacae30713e2fa550ff
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-03-22 23:58:19 +0900

    emotionの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react';
+import { css, ClassNames } from '@emotion/react';
 import './App.css';
 import { ChildArea } from './components/ChildArea';
 
@@ -6,21 +7,30 @@ function App() {
   const [text, setText] = useState('');
   const [open, setOpen] = useState(false);
 
-  const onChangeText = (e: any) => {
-    return setText(e.target.value)
-  }
+  const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return setText(e.target.value);
+  };
 
   const onClickOpen = () => {
     setOpen(!open);
-  }
+  };
 
   const onClickClose = useCallback(() => {
     setOpen(false);
-  }, [])
+  }, []);
+
+  const containerStyle = css`
+    background-color: #f0f0f0;
+    padding: 20px;
+    border-radius: 5px;
+  `;
 
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText}/>
+      <ClassNames>
+        {({ css }) => <div className={css(containerStyle)}></div>}
+      </ClassNames>
+      <input value={text} onChange={onChangeText} />
       <br />
       <button onClick={onClickOpen}>表示</button>
       <ChildArea open={open} onClickClose={onClickClose} />
