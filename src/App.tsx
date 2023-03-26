afc6c0a6a9e63021f09e2701ca5c9ed341085567
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-03-26 11:36:13 +0900

    remove: 使用しないファイルを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Router } from './router/router';
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecandaryButton } from './components/atoms/button/SecandaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <PrimaryButton>
-          test
-        </PrimaryButton>
-        <SecandaryButton>
-          test
-        </SecandaryButton>
-        <SearchInput></SearchInput>
-      </div>
-      <Router />
-    </BrowserRouter>
+    <div className="App">
+    </div>
   );
 }
 
