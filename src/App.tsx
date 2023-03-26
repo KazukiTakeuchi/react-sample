e68f2f97a90f3091db93fb6e017a8a50b977a74f
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-03-26 16:42:06 +0900

    feat: Templateの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { TaskList } from "./components/organisms/TaskList";
+import { AppTemplate } from "./components/Templates/AppTemplate";
+
 function App() {
   return (
     <div className="App">
+      <AppTemplate>
+        <TaskList />
+      </AppTemplate>
     </div>
   );
 }
